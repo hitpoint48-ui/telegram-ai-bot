8323041f61b8cfff04603ded2426550b5444974a
hitpoint48-ui <hitpoint48@gmail.com>
2026-04-24 05:12:12 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from openai import OpenAI
 
-TELEGRAM_TOKEN = "8644438760:AAGdf9vAA0ICvHsO1hyLh2RGPySEmwj9fPk"
-OPENAI_API_KEY = "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"
+TELEGRAM_TOKEN = os.getenv("8644438760:AAGdf9vAA0ICvHsO1hyLh2RGPySEmwj9fPk")
+OPENAI_API_KEY = os.getenv("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")
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
